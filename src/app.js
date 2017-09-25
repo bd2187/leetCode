11df39e246d7c9816769f17cc8331565dd524fed
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-24 20:16:11 -0700

    Revise two_sum algorithm

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ return [0, 1].
 
 function twoSum(nums, target) {
   for (let i = 0; i < nums.length; i++) {
-    for (let j = 1; j < nums.length; j++) {
+    for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
         return [i, j];
       }
